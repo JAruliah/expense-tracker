8adb104de14bb26b0d9e6ee81ae3b6c3701ef536
Jonathan <71105258+JAruliah@users.noreply.github.com>
2021-10-22 21:20:57 -0600

    connect to database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const PORT = 3001
 const app = express()
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+
+
+
+
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, () =>{
+    console.log('Connected to DB!')
+})
 
 
 app.listen(PORT, console.log(`Listening on port ${PORT}`))
