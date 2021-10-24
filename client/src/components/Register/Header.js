faa859f3b6f9d02394ff79332df7085ee632bee4
Jonathan <71105258+JAruliah@users.noreply.github.com>
2021-10-24 14:07:14 -0600

    register page

---
+++ client/src/components/Register/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+function Header() {
+    return (
+        <header>
+            <h1>Welcome</h1>
+            <Navbar />
+
+        </header>
+    )
+}
+
+
+export default Header
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,9 @@
 import React from "react";
+import Header from "../components/Register/Header";
 
 function Register(){
     return (
-        <div>
-            <h1>This is the register page</h1>
-        </div>
+        <Header />
     )
 }
 
