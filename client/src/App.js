14b0190d6c84b5a0729a2e5aff96b689d16d21e7
Jonathan <71105258+JAruliah@users.noreply.github.com>
2021-10-24 13:48:58 -0600

    react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Login from './pages/Login'
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard"
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
+import NotFoundPage from './pages/404'
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Switch>
+          <Route exact path ="/" component ={Login} />
+          <Route exact path ="/register" component ={Register} />
+          <Route exact path ="/dashboard" component ={Dashboard} />
+          <Route eact path="/404" component={NotFoundPage} />
+          <Redirect to="/404" />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Link} from 'react-router-dom'
+
+function Register(){
+    return (
+        <div>
+            <h1>This is the Login page</h1>
+            <Link to="/">Login Page</Link>
+        </div>
+    )
+}
+
+export default Register
