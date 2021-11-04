948592930d2568b7ff1c3949b6da9b64cd079c90
Jonathan Aruliah <71105258+JAruliah@users.noreply.github.com>
2021-11-04 01:06:46 -0600

    Update Form.js

--- client/src/components/Register/Form.js
+++ client/src/components/Register/Form.js
@@ -14,7 +14,7 @@ function Form(props){
         event.preventDefault()
         if(password === passwordConfirmation){
             
-            // don't remember from where i copied this code, but this works.
+            // REGEX email format
             let re = /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     
             if ( re.test(email) ) {
